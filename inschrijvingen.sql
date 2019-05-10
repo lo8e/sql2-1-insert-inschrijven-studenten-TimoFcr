5ca2dd5ebe710c06511efd5333e3987d668c1cd6
TimoFcr <1045506@mborijnland.nl>
2019-05-10 15:20:39 +0200

    opdracht 2

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -3,4 +3,10 @@ INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
 VALUES ("lo9e-amo1","akr02","2019","amo"),
 	   ("lo9e-amo2","bkr02","2019","amo"),
 	   ("lo9e-amo3","brc01","2019","amo");
-       
+
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum)
+VALUES ("90000","Barikzai",NULL,"Samir","15-10-2001","1111 AB","Leiden","105.2","2.11","16-10-2001"),
+	   ("90001","Ruiter","de","Kristian","25-11-2001","1112 BC","Leiden","104.8","2.10","26-11-2001"),
+       ("90002","Marrawi",NULL,"Hasan","15-05-1999","1113 CD","Leiden","103.5","0.56","16-05-1999"),
+       ("90003","Schouten",NULL,"Timo","19-03-2002","2224 GJ","Katwijk","72.4","1.87","20-03-2002");
+
