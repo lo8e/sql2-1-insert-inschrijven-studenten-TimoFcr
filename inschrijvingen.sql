30bfc7b53587f4de9c1d512f6dd02455249aeb69
TimoFcr <1045506@mborijnland.nl>
2019-05-10 15:02:48 +0200

    opdracht1

---
+++ inschrijvingen.sql
@@ -0,0 +1,6 @@
+USE `lo8e_sql2`;
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+VALUES ("lo9e-amo1","akr02","2019","amo"),
+	   ("lo9e-amo2","bkr02","2019","amo"),
+	   ("lo9e-amo3","brc01","2019","amo");
+       
